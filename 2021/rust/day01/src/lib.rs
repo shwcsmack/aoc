9a4918c18d46e7f02c0e8238544c18e49e7cbf4a
Shayne Stewart <sh.wc.smack@gmail.com>
2022-12-04 13:34:58 -0600

    2021day2 start

--- 2021/rust/day01/src/lib.rs
+++ 2021/rust/day01/src/lib.rs
@@ -36,16 +36,12 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "199
-    200
-    208
-    210
-    200
-    207
-    240
-    269
-    260
-    263";
+    const INPUT: &str = "forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2";
 
     #[test]
     fn part_one() {
@@ -53,6 +49,7 @@ mod tests {
         assert_eq!(result, "7");
     }
 
+    #[ignore]
     #[test]
     fn part_two() {
         let result = process_part2(INPUT);
