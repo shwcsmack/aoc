b06ff4d12f6289cbe58f11fd0b598229eac1d54f
Shayne Stewart <sh.wc.smack@gmail.com>
2022-12-05 00:51:25 -0600

    22day5p1 setup

--- 2022/rust/day05/src/lib.rs
+++ 2022/rust/day05/src/lib.rs
@@ -17,12 +17,20 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "    [D]    
+    [N] [C]    
+    [Z] [M] [P]
+     1   2   3 
+    
+    move 1 from 2 to 1
+    move 3 from 1 to 3
+    move 2 from 2 to 1
+    move 1 from 1 to 2";
 
     #[test]
     fn part_one() {
         let result = process_part1(INPUT);
-        assert_eq!(result, "0");
+        assert_eq!(result, "CMZ");
     }
 
     #[test]
