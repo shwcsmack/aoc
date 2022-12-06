11f06ccfd77c66e834b1a915204e074991070181
Shayne Stewart <sh.wc.smack@gmail.com>
2022-12-06 18:46:46 -0500

    22day6 done

--- 2022/rust/day06/src/lib.rs
+++ 2022/rust/day06/src/lib.rs
@@ -2,14 +2,34 @@
 //cargo run --bin part-2
 //cargo watch -x check -x test
 
+use std::collections::VecDeque;
 
+
+fn has_duplicates(chars_in: &[char]) -> bool {
+    let mut chars_to_check = chars_in.clone().to_vec();
+    let mut output = false;
+
+    while chars_to_check.len() > 0 && !output {
+        let char_to_check = chars_to_check.pop().unwrap();
+        output = chars_to_check.contains(&char_to_check);
+    }
+    output
+}
 
 pub fn process_part1(input: &str) -> String {
-    todo!();
+    let mut chars: Vec<char> = input.chars().collect();
+
+    let pre_marker_windows: Vec<&[char]> = chars.windows(4).take_while(|window| has_duplicates(window)).collect();
+
+    (pre_marker_windows.len() + 4).to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    todo!();
+    let mut chars: Vec<char> = input.chars().collect();
+
+    let pre_marker_windows: Vec<&[char]> = chars.windows(14).take_while(|window| has_duplicates(window)).collect();
+
+    (pre_marker_windows.len() + 14).to_string()
 }
 
 
@@ -17,17 +37,17 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
 
     #[test]
     fn part_one() {
         let result = process_part1(INPUT);
-        assert_eq!(result, "0");
+        assert_eq!(result, "7");
     }
 
     #[test]
     fn part_two() {
         let result = process_part2(INPUT);
-        assert_eq!(result, "0")
+        assert_eq!(result, "19")
     }
 }
