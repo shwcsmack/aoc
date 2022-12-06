8f7a9fa91577fa82189b678a0359752a72bcf191
Shayne Stewart <sh.wc.smack@gmail.com>
2022-12-05 21:24:06 -0500

    22day5p2 done

--- 2022/rust/day05/src/lib.rs
+++ 2022/rust/day05/src/lib.rs
@@ -50,17 +50,35 @@ impl Warehouse {
     }
 
     fn run_command(&mut self, command: Command) {
-        dbg!(command);
         for _ in 0..command.num_of_boxes {
             let temp_box = self.stacks[(command.from_idx-1) as usize].0.pop_front().unwrap();
             self.stacks[(command.to_idx - 1) as usize].0.push_front(temp_box);
         }
+    }
+
+    fn run_command_2(&mut self, command: Command) {
+        dbg!(command);
+        let mut temp_boxes: VecDeque<char> = VecDeque::new();
+        for _ in 0..command.num_of_boxes {
+            let temp_box = self.stacks[(command.from_idx-1) as usize].0.pop_front().unwrap();
+            temp_boxes.push_front(temp_box);
+            // self.stacks[(command.to_idx - 1) as usize].0.push_front(temp_box);
+        }
+        println!("temp_boxes: {:?}", temp_boxes);
+        for idx in 0..temp_boxes.len() {
+            println!("idx: {}", idx);
+            self.stacks[(command.to_idx - 1) as usize].0.push_front(temp_boxes.pop_front().unwrap());
+        }
         dbg!(self);
     }
 
-    fn run_commands(&mut self, commands: Vec<&str>) {
+    fn run_commands(&mut self, commands: Vec<&str>, part: u8) {
         for command_str in commands {
-            self.run_command(command_str.parse().unwrap());
+            match part {
+                1 => self.run_command(command_str.parse().unwrap()),
+                2 => self.run_command_2(command_str.parse().unwrap()),
+                _ => panic!("invalid part number"),
+            }
         }
     }
 
@@ -147,12 +165,45 @@ pub fn process_part1(input: &str) -> String {
     let mut warehouse = Warehouse::new(boxes, width);   
     lines_iter.next().unwrap();
     let commands: Vec<&str> = lines_iter.map(|line| line.clone()).collect();
-    warehouse.run_commands(commands);
+    warehouse.run_commands(commands, 1);
     warehouse.top_boxes()
 }
 
 pub fn process_part2(input: &str) -> String {
-    todo!();
+    let mut width = 0;
+    let mut lines_iter = input.lines();
+    let boxes: Vec<SupplyBox> = lines_iter
+        .by_ref()
+        .take_while(|line| !line.contains('1'))
+        .map(|line| {
+            let mut total_idx = 0;
+            let mut idx = 0;
+            let mut acc: Vec<char> = Vec::new();
+            let mut boxes: Vec<SupplyBox> = Vec::new();
+            line.chars().for_each(|char| {
+                if idx < 3 && total_idx < line.len()-1 {
+                    acc.push(char);
+                    idx += 1;
+                } else if  total_idx == line.len()-1{
+                    acc.push(char);
+                    boxes.push(acc.iter().collect::<String>().parse::<SupplyBox>().unwrap());
+                } else {
+                    boxes.push(acc.iter().collect::<String>().parse::<SupplyBox>().unwrap());
+                    acc.clear();
+                    idx = 0;
+                }
+                total_idx += 1;
+            });
+            width = boxes.len();
+            boxes    
+        })
+        .flatten()
+        .collect();
+    let mut warehouse = Warehouse::new(boxes, width);   
+    lines_iter.next().unwrap();
+    let commands: Vec<&str> = lines_iter.map(|line| line.clone()).collect();
+    warehouse.run_commands(commands, 2);
+    warehouse.top_boxes()
 }
 
 
@@ -176,10 +227,9 @@ move 1 from 1 to 2";
         assert_eq!(result, "CMZ");
     }
 
-    #[ignore]
     #[test]
     fn part_two() {
         let result = process_part2(INPUT);
-        assert_eq!(result, "0")
+        assert_eq!(result, "MCD")
     }
 }
