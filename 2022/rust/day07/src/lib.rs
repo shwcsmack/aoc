32e3f6715168dee9bb0368007d0353b38a2e8939
Shayne Stewart <sh.wc.smack@gmail.com>
2022-12-07 00:13:42 -0500

    22day7p1 start

--- 2022/rust/day07/src/lib.rs
+++ 2022/rust/day07/src/lib.rs
@@ -2,8 +2,6 @@
 //cargo run --bin part-2
 //cargo watch -x check -x test
 
-
-
 pub fn process_part1(input: &str) -> String {
     todo!();
 }
@@ -12,12 +10,33 @@ pub fn process_part2(input: &str) -> String {
     todo!();
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "$ cd /
+    $ ls
+    dir a
+    14848514 b.txt
+    8504156 c.dat
+    dir d
+    $ cd a
+    $ ls
+    dir e
+    29116 f
+    2557 g
+    62596 h.lst
+    $ cd e
+    $ ls
+    584 i
+    $ cd ..
+    $ cd ..
+    $ cd d
+    $ ls
+    4060174 j
+    8033020 d.log
+    5626152 d.ext
+    7214296 k";
 
     #[test]
     fn part_one() {
@@ -25,6 +44,7 @@ mod tests {
         assert_eq!(result, "0");
     }
 
+    #[ignore]
     #[test]
     fn part_two() {
         let result = process_part2(INPUT);
